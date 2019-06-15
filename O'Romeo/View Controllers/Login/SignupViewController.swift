c3dd7fff456fa9c7fb14bc1826fbd63129d24e19
Brian Hersh <bri.d.hersh@gmail.com>
2019-06-15 16:51:35 -0600

    add storyboards & UI elements

---
+++ O'Romeo/View Controllers/Login/SignupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewController.swift
+//  O'Romeo
+//
+//  Created by Brian Hersh on 6/15/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ O'Romeo/View Controllers/Main/HomeTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeTableViewController.swift
+//  O'Romeo
+//
+//  Created by Brian Hersh on 6/15/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        return cell
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
