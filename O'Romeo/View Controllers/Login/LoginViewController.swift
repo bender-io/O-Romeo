c10f9524258a9ab2a98f3169ce7499a4cc30b0c7
Brian Hersh <bri.d.hersh@gmail.com>
2019-06-16 19:25:20 -0600

    add UI imporvement

--- O'Romeo/View Controllers/Login/LoginViewController.swift
+++ O'Romeo/View Controllers/Login/LoginViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.layer.cornerRadius = 5
+    }
+    
+    // MARK: - IBActions
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
     }
 }
