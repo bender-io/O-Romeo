3255206c4c82d22f756171bd2b641867373e6629
Brian Hersh <bri.d.hersh@gmail.com>
2019-06-17 08:57:46 -0600

    add HomeVC and file cleanup

---
+++ O'Romeo/View Controllers/Home/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  O'Romeo
+//
+//  Created by Brian Hersh on 6/17/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
