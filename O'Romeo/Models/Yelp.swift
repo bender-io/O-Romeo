f9638ae8ab7cd09f359ab97bf4c7dd0501b9760c
Lo <lolearningcode@gmail.com>
2019-06-18 16:36:57 -0600

    added yelp and eventful api functions and models

---
+++ O'Romeo/Models/Yelp.swift
@@ -0,0 +1,42 @@
+//
+//  Yelp.swift
+//  O'Romeo
+//
+//  Created by Lo Howard on 6/18/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelJSON: Codable {
+    let businesses: [Yelp]
+}
+
+struct Yelp: Codable {
+    let name: String
+    let imageURL: String
+    let displayPhone: String
+    let rating: Double?
+    let location: Location
+    let price: Price?
+    
+    enum CodingKeys: String, CodingKey {
+        case name, location, rating, price
+        case imageURL = "image_url"
+        case displayPhone = "display_phone"
+    }
+}
+
+struct Location: Codable {
+    
+    let displayAddress: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case displayAddress = "display_address"
+    }
+}
+
+enum Price: String, Codable {
+    case empty = "$"
+    case price = "$$"
+}
