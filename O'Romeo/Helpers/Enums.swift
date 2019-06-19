24c289f2d6af749f0f1016989a585f8055298772
Lo <lolearningcode@gmail.com>
2019-06-19 10:05:56 -0600

    added segmented control on the homevc

---
+++ O'Romeo/Helpers/Enums.swift
@@ -0,0 +1,17 @@
+//
+//  Enums.swift
+//  O'Romeo
+//
+//  Created by Lo Howard on 6/19/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import Foundation
+
+enum EventType: String {
+    //nightlife food culture music
+    case nightLife = "nightlife"
+    case food = "food"
+    case culture = "culture"
+    case music = "music"
+}
