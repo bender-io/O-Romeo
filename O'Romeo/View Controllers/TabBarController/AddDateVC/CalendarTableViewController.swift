5a130cde3e24e3401ec63787a1149fba4351fffd
Will Morris <william.morris117@gmail.com>
2019-06-26 14:27:14 -0600

    adds delete capabilities

--- O'Romeo/View Controllers/TabBarController/AddDateVC/CalendarTableViewController.swift
+++ O'Romeo/View Controllers/TabBarController/AddDateVC/CalendarTableViewController.swift
@@ -21,7 +21,6 @@ class CalendarTableViewController: UITableViewController {
         loadData()
     }
     
-    // STILL NOT WORKING
     func loadData() {
         let dispatchGroup = DispatchGroup()
         DateLogController.shared.dateLogs = []
@@ -38,8 +37,8 @@ class CalendarTableViewController: UITableViewController {
         dispatchGroup.notify(queue: .main) {
             DispatchQueue.main.async {
                 self.dateLogs = DateLogController.shared.dateLogs
+                self.dateLogs.sort(by: { $0.date.asDate() < $1.date.asDate() })
                 self.tableView.reloadData()
-                
             }
         }
     }
@@ -51,41 +50,39 @@ override func tableView(_ tableView: UITableView, heightForRowAt indexPath: Inde
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return DateLogController.shared.dateLogs.count
+        return dateLogs.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "datesCell", for: indexPath) as! CalendarTableViewCell
         
-        let sortedDates = dateLogs.sorted(by: { $0.date.asDate() < $1.date.asDate() })
-        let date = sortedDates[indexPath.row]
-        cell.dateLog = date
+        let dateLog = dateLogs[indexPath.row]
+        cell.dateLog = dateLog
 
         return cell
     }
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            let dateLog = dateLogs[indexPath.row]
+            DateLogController.shared.deleteDateLog(dateLog: dateLog) { (error) in
+                if let error = error {
+                    print("There was an error deleting the datelog: \(error) : \(#function)")
+                }
+            }
+            dateLogs.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-    */
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
         if segue.identifier == "editDateDetailVC" {
             if let destinationVC = segue.destination as? AddDateViewController, let indexPath = tableView.indexPathForSelectedRow {
-                let sortedDates = dateLogs.sorted(by: { $0.date < $1.date })
-                let date = sortedDates[indexPath.row]
-                destinationVC.dateLog = date
+                let dateLog = dateLogs[indexPath.row]
+                destinationVC.dateLog = dateLog
             }
         }
     }
